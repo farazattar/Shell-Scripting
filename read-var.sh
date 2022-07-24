052a87f75ae39abfa9d11913b4050c3ff2fe4d46
farazattar <farazattar1985@gmail.com>
2022-07-24 12:05:32 +0430

    Read: Variable Parameter

---
+++ read-var.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Please enter your name:"
+read -p "Please enter your name!" -n 3 -s Name
+echo "Your name is $Name."
+read -p "first var" -n 1 var1
+read -p "second var" -n 1 var2
